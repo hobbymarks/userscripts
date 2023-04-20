68d7b11867d45938c63dc83062608fb2b0ad531b
hobbymarks <ihobbymarks@gmail.com>
2023-04-20 17:31:14 +0800

    correct match url,update version number

--- POE.com.view.user.js
+++ POE.com.view.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         POE.com.View
 // @namespace    https://github.com/hobbymarks
-// @version      1.0.1
+// @version      1.0.2
 // @description  Tampermonkey script for Chrome to view POE.com
 // @author       hobbymarks
+// @match        https://poe.com/
 // @match        https://poe.com/*
-// @match        https://poe.com/*/
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=poe.com
 // @grant        GM_addStyle
 // ==/UserScript==
